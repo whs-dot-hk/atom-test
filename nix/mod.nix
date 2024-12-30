a69962b4065b8a0fc271a44265bce5f33e1a5e4a
whs <hswongac@gmail.com>
2024-12-30 08:59:53 +0800

    Update

--- nix/mod.nix
+++ nix/mod.nix
@@ -2,7 +2,7 @@
   Test = atom.pkgs.rustPlatform.buildRustPackage {
     pname = "test";
     version = "0.1.0";
-    src = atom.all;
+    src = atom;
     cargoHash = "sha256-Lj1bsc52QLivvAW5rHbU1n/stZLPuXZ8TSScUOtANjg=";
   };
 }
